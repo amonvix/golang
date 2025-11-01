7fb726039d2a22c5752ec9b74d30b8b07db06835
Amonvix <hspedroso@gmail.com>
2025-11-01 16:04:21 -0300

    Learnig path in Golang started

---
+++ format_printing/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name = "Amon"
+	const age = 40
+
+	fmt.Printf("This guy named %s is now %d years old. \t and the type is %T and %T", name, age, name, age)
+}

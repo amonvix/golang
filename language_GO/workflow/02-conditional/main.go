0eb5c2386f36ff57bb349cdc6ae8a9ba2a562aa0
amonvix <hspedroso@gmail.com>
2026-02-16 21:11:06 -0300

    workflow: files organization made to a better understanding

--- language_GO/workflow/02-conditional/main.go
+++ language_GO/workflow/02-conditional/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-
-	x := 45
-	y := 5
-	fmt.Printf(" x=%v \n y=%v", x, y)
+	//SEQUENCE
+	fmt.Println("this is the first statement to run")
+	fmt.Println("this is the second statement to run")
+	x := 45 // this is the third statement to run
+	y := 5  // this is the fourth statement to run
+	fmt.Printf(" x=%v \n y=%v\n", x, y)
 
 	// CONDITIONAL
 	//  if statements
@@ -36,9 +38,21 @@ func main() {
 	}
 	/*
 		"If" statements specify the conditional execution of two branches
-		according to the value of boolean expression. If the expression evaluates
-		to true, the if" branch is executed, otherwise, if present, the else "branch" is executed
+		according to the value of a boolean expression. If the expression evaluates
+		to true, the "if" branch is executed, otherwise, if present, the "else" branch is executed.
 	*/
-	//  https://go.dev/ref/spec#If_statements
+	// https://go.dev/ref/spec#If_statements
 
+	/*
+		Comparison operators
+		Comparison operators compare two operands and yield an untyped boolean value.
+
+		==    equal
+		!=    not equal
+		<     less
+		<=    less or equal
+		>     greater
+		>=    greater or equal
+	*/
+	// https://go.dev/ref/spec#Comparison_operators
 }
